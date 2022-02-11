476cd632ef9126fa6cf9a79b5e22d80e6bc4708c
Eze Vazquez <yourgitemail@example.com>
2022-02-10 21:08:37 -0300

    creación de home page

--- CoderBlog/urls.py
+++ CoderBlog/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include('homepage.urls'), name=""),
     path("accounts/", include('accounts.urls'), name=""),
     path("pages/", include('pages.urls'), name=""),
 ]
---
+++ homepage/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def homepage(req):
+ return render(req, 'homepage/index.html')
+ 
